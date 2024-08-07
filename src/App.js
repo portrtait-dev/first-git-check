2fe6b600255d4b13571eecce91542b069e604854
Olzhas <olzhas.dossaibek.dev@gmail.com>
2024-08-07 12:36:19 +0500

    commit via VSCode, not terminal

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h2>Selamlar - only master branch</h2>
+        <h2>Selamlar - create new branch as well</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
